f309f27437e2ef0cd6cf9bc82d30dc47358cd337
Анатолий Казаков <frankismile@yandex.ru>
2022-10-15 21:29:29 +0700

    Страдания! Теперь траты разделены по категориям, к спискам добавил мапы

--- ExpensesManager.java
+++ ExpensesManager.java
@@ -1,59 +1,58 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ExpensesManager {
-    ArrayList<Double> expenses;
+    HashMap<String, ArrayList<Double>> expensesByCategories;
+    ArrayList<Double> newExpense;
 
     ExpensesManager() {
-        expenses = new ArrayList<>();
+        expensesByCategories = new HashMap<>();
+        newExpense = new ArrayList<>();
     }
 
-    double saveExpense(double moneyBeforeSalary, double expense) {
+    double saveExpense(double moneyBeforeSalary, double expense, String category) {
         moneyBeforeSalary = moneyBeforeSalary - expense;
-        expenses.add(expense);
         System.out.println("Значение сохранено! Ваш текущий баланс в рублях: " + moneyBeforeSalary);
+        if (expensesByCategories.containsKey(category)) {
+            newExpense.add(expense);
+        } else {
+            newExpense = new ArrayList<>();
+            newExpense.add(expense);
+            expensesByCategories.put(category, newExpense);
+        }
         if (moneyBeforeSalary < 1000) {
             System.out.println("На вашем счету осталось совсем немного. Стоит начать экономить!");
         }
         return moneyBeforeSalary;
     }
 
-    void printAllExpenses() {
-        for (int i = 0; i < expenses.size(); i++) {
-            System.out.println("Трата № " + (i + 1) + ". Потрачено " + expenses.get(i) + " рублей");
+    void printAllExpensesByCategories() {
+        for (String category : expensesByCategories.keySet()) {
+            System.out.println(category);
+            for (Double expense : expensesByCategories.get(category)) {
+                System.out.println(expense);
+            }
         }
     }
 
-    double findMaxExpense() {
+    double findMaxExpenseInCategory(String category) {
         double maxExpense = 0;
-        for (Double exp : expenses) {
-            if (exp > maxExpense) {
-                maxExpense = exp;
+        if (!expensesByCategories.containsKey(category)) {
+            System.out.println("Такой категории пока нет.");
+        } else {
+            for (Double exp : expensesByCategories.get(category)) {
+                if (exp > maxExpense) {
+                    maxExpense = exp;
+                }
             }
+            System.out.println("Самая большая трата в категории " + category + " составила "
+                    + maxExpense + " руб.");
         }
         return maxExpense;
     }
 
     void removeAllExpenses() {
-        expenses.clear();
-        System.out.println("Список трат пуст.");
-    }
-
-
-    void removeExpense(double expense) {
-        if (expenses.isEmpty()) {
-            System.out.println("Список трат пуст.");
-        } else if (expenses.contains(expense)) {
-            int index = 0;
-            for (int i = 0; i < expenses.size(); i++) {
-                if (expenses.get(i) == expense) {
-                    index = i;
-                    break;
-                }
-            }
-            expenses.remove(index);
-            System.out.println("Трта удалена!");
-        } else {
-            System.out.println("Такой траты нет.");
-        }
+        expensesByCategories.clear();
+        System.out.println("Траты удалены.");
     }
 }
